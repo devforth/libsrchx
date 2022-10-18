08e79dc29f3835536a30d6e3c9d1868ac1f22050
LbP22 <aakopyan@devforth.io>
2022-10-18 13:58:06 +0300

    boltdb and leveldb removed

--- index.go
+++ index.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/blevesearch/bleve"
 	"github.com/imdario/mergo"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 )
 
 // Index - the index wrapper
@@ -51,10 +51,7 @@ func (i *Index) Get(id string) (map[string]interface{}, error) {
 // Put - set/update a document
 func (i *Index) Put(data map[string]interface{}) (document map[string]interface{}, err error) {
 	if _, found := data["id"]; !found || data["id"] == "" {
-		uid, err := uuid.NewV4()
-		if err != nil {
-			return nil, err
-		}
+		uid := uuid.NewV4()
 		data["id"] = uid.String()
 		data["created_at"] = time.Now().UnixNano()
 	}
